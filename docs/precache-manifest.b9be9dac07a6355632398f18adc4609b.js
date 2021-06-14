4d59e673f67d5ceac45559b9da9158bdaff3523d
Alejandro Perez-canto <alejandroperezcanto@gmail.com>
2021-06-14 14:22:15 -0400

    docs

---
+++ docs/precache-manifest.b9be9dac07a6355632398f18adc4609b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81e730a6a9233a0c0b644059eb51913",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d29b9ce02cc373cca17",
+    "url": "/static/css/main.8d341183.chunk.css"
+  },
+  {
+    "revision": "13255f3ef07995ce48e0",
+    "url": "/static/js/2.2bfe2479.chunk.js"
+  },
+  {
+    "revision": "55b79ebb5e774a9f9242c9bbc135e8e0",
+    "url": "/static/js/2.2bfe2479.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d29b9ce02cc373cca17",
+    "url": "/static/js/main.40f00a4e.chunk.js"
+  },
+  {
+    "revision": "3d4175f9165103e13d56",
+    "url": "/static/js/runtime-main.b8be2bc2.js"
+  }
+]);
